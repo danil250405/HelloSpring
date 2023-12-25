78c90a5a9df31284400d90d44bb5b331fc0ec6a7
danil250405 <danilg250405@gmail.com>
2023-12-25 19:07:03 +0100

    lesson 1 spring new branch

--- src/main/java/com/glazweq/springapp/TestSpring.java
+++ src/main/java/com/glazweq/springapp/TestSpring.java
@@ -3,6 +3,7 @@ package com.glazweq.springapp;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
+
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
